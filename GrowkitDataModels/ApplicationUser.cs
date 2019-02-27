36ba8a9feb65d15f663ba28d5e9678c8bf73314f
Pheubel <luca.guagliardo@live.nl>
2019-02-27 11:54:19 +0100

    Updated data models
    -replaced user model with extended identity user model.
    -added friends model

--- GrowkitDataModels/ApplicationUser.cs
+++ GrowkitDataModels/ApplicationUser.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace GrowkitDataModels
 {
     /// <summary> A subscriber model of a user of Vodaphone-Ziggo.</summary>
-    public class User
+    public class ApplicationUser : IdentityUser<Guid>
     {
-        /// <summary> The unique user identifying number.</summary>
-        [Key] public Guid UserId { get; set; }
-        /// <summary> The name displayed to other users.</summary>
-        public string Username { get; set; }
         /// <summary> The hub currently registered on this user.</summary>
         /// <remarks>when going full scale, this should be added via inherritance or another method as multiple services might be added later.</remarks>
         public virtual ICollection<GKHub> OwnedHubs { get; set; }
