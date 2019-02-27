242d96ab258c164c4e658b01f87af26999ac80ba
Pheubel <luca.guagliardo@live.nl>
2019-02-27 13:59:30 +0100

    Added schaffolded identity pages
    -scaffolding added for more control over indiviual pages

---
+++ Growkit website/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using Growkit_website.Data;
+//using GrowkitDataModels;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+
+//[assembly: HostingStartup(typeof(Growkit_website.Areas.Identity.IdentityHostingStartup))]
+//namespace Growkit_website.Areas.Identity
+//{
+
+//    public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) => {
+//            });
+//        }
+//    }
+//}
