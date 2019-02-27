d86ae770c4f6a292dcd9addb4eddcd742eed1d80
Pheubel <luca.guagliardo@live.nl>
2019-02-27 14:17:16 +0100

    Updated model
    -updated friends to make the foreign key relations required
    -updated plantpreset to make a name required

--- GrowkitDataModels/ApplicationUser.cs
+++ GrowkitDataModels/ApplicationUser.cs
@@ -9,7 +9,7 @@ namespace GrowkitDataModels
     public class ApplicationUser : IdentityUser<Guid>
     {
         /// <summary> The hub currently registered on this user.</summary>
-        /// <remarks>when going full scale, this should be added via inherritance or another method as multiple services might be added later.</remarks>
+        /// <remarks> when going full scale, this should be added via inherritance or another method as multiple services might be added later.</remarks>
         public virtual ICollection<GKHub> OwnedHubs { get; set; }
     }
 }
--- GrowkitDataModels/Friends.cs
+++ GrowkitDataModels/Friends.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace GrowkitDataModels
@@ -9,13 +10,17 @@ namespace GrowkitDataModels
     public class Friends
     {
         /// <summary> The unique identity of this relation.</summary>
-        [Key]public ulong RelationId { get; set; }
+        [Key] public ulong RelationId { get; set; }
         /// <summary> Determines if the friend request has been accepted by the other user.</summary>
         public bool IsConfirmed { get; set; }
+        /// <summary> The Id of the user that sent the friend request.</summary>
+        public Guid RequestSenderId { get; set; }
+        /// <summary> The Id of the user that recieved the friend request.</summary>
+        public Guid RequestRecieverId { get; set; }
 
         /// <summary> The user that sent the friend request.</summary>
-        public virtual ApplicationUser FriendFrom { get; set; }
-        /// <summary> The User that recieved the friend request.</summary>
-        public virtual ApplicationUser FriendTo { get; set; }
+        [ForeignKey("RequestSenderId")] public virtual ApplicationUser RequestSender { get; set; }
+        /// <summary> The user that recieved the friend request.</summary>
+        [ForeignKey("RequestRecieverId")] public virtual ApplicationUser RequestReciever { get; set; }
     }
 }
