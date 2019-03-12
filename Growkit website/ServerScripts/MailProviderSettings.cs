621d43ec4190d6b182c3ed0f4e202a1dd0645022
Pheubel <luca.guagliardo@live.nl>
2019-03-12 10:23:26 +0100

    Fixed MailProvidelSettings model
    -values can be read from appsettings now.

--- Growkit website/ServerScripts/MailProviderSettings.cs
+++ Growkit website/ServerScripts/MailProviderSettings.cs
@@ -4,16 +4,16 @@
     public class MailProviderSettings
     {
         /// <summary> The email adress of the sender.</summary>
-        public string SenderAdress { get; }
+        public string SenderAdress { get; set; }
         /// <summary> The username used for network credentials.</summary>
         public string Username { get; set; }
         /// <summary> The password used for network credentials.</summary>
         public string Password { get; set; }
         /// <summary> The server hosting the email service.</summary>
-        public string Host { get; }
+        public string Host { get; set; }
         /// <summary> The port configured on the server for sending email messages.</summary>
-        public int Port { get; }
+        public int Port { get; set; }
         /// <summary> Specifies if the Secule Socket Layer (SSL) will be used.</summary>
-        public bool EnableSsl { get; }
+        public bool EnableSsl { get; set; }
     }
 }
