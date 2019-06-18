338c89e747fdd2c1a8d1e3fadd44a4162228a483
Pheubel <luca.guagliardo@live.nl>
2019-06-18 14:32:54 +0200

    created and tested migrations

---
+++ GrowKitApi/Migrations/Authentication/20190618120931_InitialAuthentication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace GrowKitApi.Migrations.Authentication
+{
+    public partial class InitialAuthentication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    NormalizedEmailAdress = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true),
+                    AuthenticatorSecret = table.Column<byte[]>(nullable: true),
+                    PreviousTimeWindow = table.Column<long>(nullable: false),
+                    Flags = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmailConfirmationRequests",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false),
+                    ConfirmationToken = table.Column<Guid>(nullable: false),
+                    ExpirationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailConfirmationRequests", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_EmailConfirmationRequests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailConfirmationRequests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
